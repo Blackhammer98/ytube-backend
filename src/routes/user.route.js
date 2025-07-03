e7de2327241e73bd1fe54e4a3e8954ed6428b7c3
Your Name <you@example.com>
2025-07-04 00:55:04 +0530

    add login and logout functionallity , auth middleware , debug middleware errors

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const userRouter = Router();
 
 userRouter.route("/register").post(
@@ -16,5 +18,11 @@ userRouter.route("/register").post(
     ]),
     registerUser
 )
+
+userRouter.route("/login").post(loginUser)
+
+// secured Routes
+
+userRouter.route("/logout").post(verifyJWT, logoutUser)
 
 export default userRouter;
